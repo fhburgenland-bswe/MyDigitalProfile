2b264b830facec4c42af1f19f0d7ae825a35a6fb
Martin Dworak <2210859002@fh-burgenland.at>
2024-04-10 16:49:23 +0200

    deleted unused files, added first version of login page design (desktop only), relates to #BD-23

---
+++ BDO-Frontend/src/LoginPage.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  
+  <div class="LoginContainer">
+    <div id="bdo-logo"><img src="/src/assets/bdologo.png"></div>
+    <div class="logintext">In Account einloggen</div>
+
+    <form action="" method="post">
+      <label for="email">E-Mail</label>
+      <div class="input-box"><input type="email" name="" id="" placeholder="max.muster@bdo.de" required></div>
+      <label for="password">Passwort</label>
+      <div class="input-box"><input type="password" name="" id="" placeholder="Enter your password" required></div>
+      <input type="button" value="Login">
+    </form>
+
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
