a1afcb1a71d986a06b9b1d2e5c51167efbd1cf95
Martin Dworak <2210859002@fh-burgenland.at>
2024-04-13 19:20:07 +0200

    added grid for user data , closes #BD-25

--- BDO-Frontend/src/components/MainPage/MainPage.vue
+++ BDO-Frontend/src/components/MainPage/MainPage.vue
@@ -6,7 +6,6 @@ import '@/assets/MainPage.css';
 <template>
 
   <header>
-    <div class="container">
       <nav>
         <ul>
           <div class="btn">
@@ -20,25 +19,115 @@ import '@/assets/MainPage.css';
           <img src="/src/assets/bdologo.png" alt="" />
         </div>
       </nav>
-    </div>
 
-    <main>
-
-    </main>
   </header>
+    <main>
+      <div class="container">
+        <div class="grid-item">Personalnummer</div>
+        <div class="grid-item">1234567890</div>
+        <div class="grid-item"></div>
+      </div>
+
+      <div class="container">
+        <div class="grid-item">Vorname</div>
+        <div class="grid-item">Max</div>
+        <div class="grid-item"></div>
+      </div>
+
+      <div class="container">
+        <div class="grid-item">Nachname</div>
+        <div class="grid-item">Muster</div>
+        <div class="grid-item"></div>
+      </div>
+
+      <div class="container">
+        <div class="grid-item">Geburtsdatum</div>
+        <div class="grid-item">01.01.2000</div>
+        <div class="grid-item"></div>
+      </div>
+
+      <div class="container">
+        <div class="grid-item">Standort</div>
+        <div class="grid-item">Düsseldorf</div>
+        <div class="grid-item"><img src="/src/assets/pencil.svg" alt="Pencil Icon" srcset=""></div>
+      </div>
+
+      <div class="container">
+        <div class="grid-item">PLZ</div>
+        <div class="grid-item">1010</div>
+        <div class="grid-item"><img src="/src/assets/pencil.svg" alt="Pencil Icon" srcset=""></div>
+      </div>
+
+      <div class="container">
+        <div class="grid-item">Ort</div>
+        <div class="grid-item">Wien</div>
+        <div class="grid-item"><img src="/src/assets/pencil.svg" alt="Pencil Icon" srcset=""></div>
+      </div>
+
+      <div class="container">
+        <div class="grid-item">Straße</div>
+        <div class="grid-item">Hauptstraße</div>
+        <div class="grid-item"><img src="/src/assets/pencil.svg" alt="Pencil Icon" srcset=""></div>
+      </div>
+
+      <div class="container">
+        <div class="grid-item">Hausnummer</div>
+        <div class="grid-item">12/3/2</div>
+        <div class="grid-item"><img src="/src/assets/pencil.svg" alt="Pencil Icon" srcset=""></div>
+      </div>
+
+      <div class="container">
+        <div class="grid-item">Skills</div>
+        <div class="grid-item">Java</div>
+        <div class="grid-item"></div>
+      </div>
+
+      <div class="container">
+        <div class="grid-item">Karrierelevel</div>
+        <div class="grid-item">Junior</div>
+        <div class="grid-item"></div>
+      </div>
+    </main>
+
 
 
 </template>
 
 <style scoped>
-
-
 * {
   font-family: Poppins, serif;
   padding: 0;
   margin: 0;
   box-sizing: border-box;
 }
+
+main{
+  margin: 100px auto;
+  max-width: 600px;
+  display: grid;
+  grid-template-columns: min-content min-content min-content; /* Three columns with different widths */
+  grid-template-rows: repeat(11, auto); /* 11 rows to accommodate all items */
+  align-items: center;
+  justify-content: center;
+  font-size: 1.5rem;
+  gap: 20px;
+}
+
+.container{
+  display: contents;
+}
+
+
+.grid-item:nth-child(2) {
+  text-align: right;
+  margin-left: 40px;
+}
+
+.grid-item:nth-child(3) {
+  justify-self: start;
+  width: fit-content;
+}
+
 
 body {
   min-height: 100vh;
@@ -56,7 +145,6 @@ nav {
   display: flex;
   justify-content: space-between;
   align-items: center;
-
 }
 
 nav .logo {
