ea406b6b7331dd2d0bfe3be63a8c1541be0eef7c
Martin Dworak <2210859002@fh-burgenland.at>
2024-04-25 19:47:56 +0200

    installed toastify so a pretty notification popup appears when the user entered the wrong login credentials, relates to #BD-35

--- BDO-Frontend/src/main.ts
+++ BDO-Frontend/src/main.ts
@@ -1,10 +1,16 @@
 import './assets/main.css'
+import 'vue3-toastify/dist/index.css';
 
 import { createApp } from 'vue'
 import router from './router'
 import App from './App.vue'
+import Vue3Toastify from "vue3-toastify";
+
 
 
 createApp(App)
     .use(router)
+    .use(Vue3Toastify, {
+        multiple: false,
+    })
     .mount('#app')
