0078f61a6331d02a738e1004251ce44b6ba87daa
Martin Dworak <2210859002@fh-burgenland.at>
2024-04-28 19:06:53 +0200

    installed vitest coverage and configured the test script in package-json to get a coverage report after running the unit tests

--- BDO-Frontend/vitest.config.ts
+++ BDO-Frontend/vitest.config.ts
@@ -6,6 +6,12 @@ export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
+        coverage:{
+            enabled: true,
+            reporter: ['text', 'json', 'html'],
+            include: ['src/**/*.ts', 'src/**/*.vue'],
+            exclude: ['node_modules', 'tests', 'src/main.ts', 'src/**/*.d.ts']
+        },
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/*'],
       root: fileURLToPath(new URL('./', import.meta.url))
