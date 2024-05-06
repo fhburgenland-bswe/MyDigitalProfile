571117fa1125fc8ea567b9b7f59f740811c3c9a1
Khaled <2210859032@fh-burgenland.at>
2024-05-06 19:41:53 +0200

    feat(Mitarbeiter): update repository
    - Update queries in MitarbeiterRepository for optimal data retrieval

--- BDO-Backend/src/main/java/mydigitalprofile/repository/MitarbeiterRepository.java
+++ BDO-Backend/src/main/java/mydigitalprofile/repository/MitarbeiterRepository.java
@@ -1,11 +1,36 @@
 package mydigitalprofile.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import mydigitalprofile.model.Mitarbeiter;
+import mydigitalprofile.model.dto.MitarbeiterDto;
 
 @Repository
 public interface MitarbeiterRepository extends JpaRepository<Mitarbeiter, Long> {
+
+
+    public Mitarbeiter findByUsername(String username);
+
+    @Query(value = "SELECT new mydigitalprofile.model.dto.MitarbeiterDto( "
+            + "m.pnr, m.vorname, m.nachname, m.username, "
+            + "a.strasse, a.hausNr, a.plz, a.ort, m.standort, m.rolle, m.karriereLevel) "
+            + "FROM Mitarbeiter m "
+            + "JOIN m.address a "
+            + "WHERE m.maId=:id "
+            + "AND m.username=:username")
+    public MitarbeiterDto findByIdAndUsername(@Param(value = "username") String username, @Param(value = "id") long id);
+
+
+    @Query(value = "SELECT new mydigitalprofile.model.dto.MitarbeiterDto( "
+            + "m.pnr, m.vorname, m.nachname, m.username, "
+            + "a.strasse, a.hausNr, a.plz, a.ort, m.standort, m.rolle, m.karriereLevel) "
+            + "FROM Mitarbeiter m "
+            + "JOIN m.address a")
+    public List<MitarbeiterDto> findAllMitarbeiter();
 
 }
