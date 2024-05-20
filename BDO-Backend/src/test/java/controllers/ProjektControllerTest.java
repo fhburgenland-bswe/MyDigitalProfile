2c648bc8408a6d751b24c49c11ee0c94690fd46f
Khaled <2210859032@fh-burgenland.at>
2024-05-20 20:41:56 +0200

    feat: implement Swagger API documentation - relates to BD-110
    - Create SwaggerConfiguration for API documentation setup
    - Add necessary Swagger dependencies in pom.xml
    - Update MitarbeiterController, MitarbeiterRepository, and MitarbeiterService for Swagger integration
    - Modify WebSecurityConfig
    - Adjust tests to accommodate Swagger changes

--- BDO-Backend/src/test/java/controllers/ProjektControllerTest.java
+++ BDO-Backend/src/test/java/controllers/ProjektControllerTest.java
@@ -8,7 +8,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
