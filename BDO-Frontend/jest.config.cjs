369517136b77f13958c60eb37916310124645e95
Basel Salem <basel.salem@hoval.com>
2024-09-23 21:37:56 +0200

    Aktiviert die Sammlung von Testabdeckungsdaten in Jest

--- BDO-Frontend/jest.config.cjs
+++ BDO-Frontend/jest.config.cjs
@@ -1,3 +1,16 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest/presets/no-babel'
-}
+  // preset: '@vue/cli-plugin-unit-jest/presets/no-babel',
+  testMatch: [
+    "<rootDir>/tests/unit/CreateUserModal.test.ts",
+    "<rootDir>/tests/unit/LoginPage.test.ts",
+    "<rootDir>/tests/unit/MainPage.test.ts",
+    "<rootDir>/tests/unit/UpdateDataModal.test.ts",
+    "<rootDir>/tests/unit/user.service.test.ts",
+  ],
+  transform: {
+    '^.+\\.ts?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+};
